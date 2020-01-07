562d3247090972c0c20cbb2e4f12b860ae3ca216
kimseonghun <seonghun127@woowahan.com>
2020-01-07 15:23:47 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/woowabros/pilotproject/PilotProjectApplication.java
@@ -0,0 +1,13 @@
+package com.woowabros.pilotproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilotProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PilotProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woowabros/pilotproject/PilotProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woowabros.pilotproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PilotProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
